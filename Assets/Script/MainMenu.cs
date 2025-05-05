5f5f842cde9dc2aa494e88f9b4e03c7f001653e3
Machima <machima.nant@bumail.net>
2025-05-06 05:44:07 +0700

    manu

---
+++ Assets/Script/MainMenu.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("GameScene"); // เปลี่ยนชื่อซีนตามที่เธอใช้จริง
+    }
+
+    public void OpenSettings()
+    {
+        SceneManager.LoadScene("SettingsScene"); // หรือใช้ Panel ก็ได้ ถ้าอยู่ในหน้าเดียวกัน
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("Game is exiting...");
+    }
+}
