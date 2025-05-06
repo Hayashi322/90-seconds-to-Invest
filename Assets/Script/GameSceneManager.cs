a0845c0f46c191d61e52bbaf2523d84647f63aa6
Machima <machima.nant@bumail.net>
2025-05-07 03:16:03 +0700

    LobbyScene

---
+++ Assets/Script/GameSceneManager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class GameSceneManager : MonoBehaviour
+{
+    public TMP_Text playerNameText;
+    public Image characterImage;
+    public Sprite[] characterSprites;
+
+    void Start()
+    {
+        if (PlayerData.Instance == null)
+        {
+            Debug.LogError("PlayerData.Instance ไม่ถูกสร้าง ตรวจสอบว่าเริ่มจาก MainMenu หรือไม่");
+            return;
+        }
+
+        // แสดงชื่อผู้เล่น
+        playerNameText.text = PlayerData.Instance.playerName;
+
+        // แสดงตัวละครตาม index ที่เลือกไว้
+        int index = PlayerData.Instance.selectedCharacterIndex;
+        if (index >= 0 && index < characterSprites.Length)
+        {
+            characterImage.sprite = characterSprites[index];
+        }
+        else
+        {
+            Debug.LogWarning("Index ตัวละครไม่ถูกต้อง");
+        }
+    }
+}
---
+++ Assets/Script/LobbyManager.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class LobbyManager : MonoBehaviour
+{
+    public TMP_Text playerNameText;
+    public Image characterPreviewImage;
+    public Button[] characterButtons;
+    public Sprite[] characterSprites;
+
+    private int selectedIndex = -1;
+
+    void Start()
+    {
+        playerNameText.text = PlayerData.Instance.playerName;
+        characterPreviewImage.sprite = null; // ไม่มีรูปเริ่มต้น
+        characterPreviewImage.color = new Color(1, 1, 1, 0); // โปร่งใส
+    }
+
+
+    public void SelectCharacter(int index)
+    {
+        selectedIndex = index;
+
+        for (int i = 0; i < characterButtons.Length; i++)
+        {
+            characterButtons[i].interactable = i != index;
+        }
+
+        characterPreviewImage.sprite = characterSprites[index];
+        characterPreviewImage.color = Color.white; // ทำให้กลับมาเห็นชัด
+        PlayerData.Instance.selectedCharacterIndex = index;
+    }
+
+
+    public void StartGame()
+    {
+        if (selectedIndex == -1)
+        {
+            Debug.Log("กรุณาเลือกตัวละครก่อนเริ่มเกม");
+            return;
+        }
+
+        SceneManager.LoadScene("GameScene");
+    }
+
+    public void ExitLobby()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
