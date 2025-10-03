9ddfd012fed31e82a15adc7adb1c96cc4964011e
Phimphisa159 <phimphisa.sris@bumail.net>
2025-10-04 00:49:39 +0700

    add Round UI

--- Assets/Script/Timer/Timer.cs
+++ Assets/Script/Timer/Timer.cs
@@ -1,4 +1,5 @@
-﻿using TMPro;
+﻿using System;
+using TMPro;
 using Unity.Netcode;
 using Unity.VisualScripting;
 using UnityEngine;
@@ -11,6 +12,8 @@ public class Timer : NetworkBehaviour
     // private NetworkVariable<CanvasGroup[]> canvases = new NetworkVariable<CanvasGroup[]>( NetworkVariableReadPermission.Everyone);
     [SerializeField] private CanvasGroup[] canvases;
     public TextMeshProUGUI countdownText;
+
+    public TextMeshProUGUI roundText;
 
     private bool timeUpTriggered = false;
 
@@ -61,24 +64,68 @@ public class Timer : NetworkBehaviour
         // เปลี่ยนเวลาตามรอบ
         if (roundCount.Value == 1)
         {
-            currentDuration.Value = 90f;
-            
+            currentDuration.Value = 60f;
+            roundText.text = "Round:1 Phase:1";
+
         }
         else if (roundCount.Value == 2)
         {
+            currentDuration.Value = 10f;
+            roundText.text = "Round:1 Phase:2";
+        }
+        else if (roundCount.Value == 3)
+        {
+            currentDuration.Value = 10f;
+            roundText.text = "Round:1 Phase:3";
+        }
+        else if (roundCount.Value == 4)
+        {
             currentDuration.Value = 30f;
             canvases[0].alpha = 1; canvases[0].blocksRaycasts = true; canvases[0].interactable = true;
+            roundText.text = "Round:2 Phase:1";
         }
-        else
+        else if (roundCount.Value == 5)
+        {
+            currentDuration.Value = 10f;
+            roundText.text = "Round:2 Phase:2";
+        }
+        else if (roundCount.Value == 6)
+        {
+            currentDuration.Value = 10f;
+            roundText.text = "Round:2 Phase:3";
+        }
+        else if (roundCount.Value == 7)
         {
             currentDuration.Value = 15f;
             canvases[0].alpha = 0; canvases[0].blocksRaycasts = false; canvases[0].interactable = false;
             canvases[1].alpha = 1; canvases[1].blocksRaycasts = true; canvases[1].interactable = true;
+            roundText.text = "Round:3 Phase:1";
+        }
+        else if (roundCount.Value == 8)
+        {
+            currentDuration.Value = 10f;
+            roundText.text = "Round:3 Phase:2";
+        }
+        else if (roundCount.Value == 9)
+        {
+            currentDuration.Value = 10f;
+            roundText.text = "Round:3 Phase:3";
+        }
+        else
+        {
+            roundText.text = "The END";
         }
 
         startTime.Value = NetworkManager.Singleton.ServerTime.Time;
         Debug.Log($"⏱️ Start Round {roundCount.Value} with {currentDuration} seconds");
     }
 
+    public void closeUI()
+    {
+        for (int i = 0; i < canvases.Length; i++)
+        {
+            canvases[i].alpha = 0; canvases[i].blocksRaycasts = false; canvases[i].interactable = false;
+        }
+    }
 }
 
--- Assets/Script/casino/casinoUI.cs
+++ Assets/Script/casino/casinoUI.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using Unity.VisualScripting;
 public class casinoUI : MonoBehaviour
 {
     [SerializeField] private bool is10000 = true;
@@ -17,8 +18,24 @@ public class casinoUI : MonoBehaviour
     [SerializeField] private bool hightOdd = false;
     [SerializeField] private bool lowEven = false;
     [SerializeField] private bool lowOdd = false;
-     public void rollDice()
+
+    [SerializeField] private int cost ;
+
+    public InventoryManager inventoryManager;
+
+    private void Start()
     {
+      //  money.text = $"{InventoryManager.Instance.cash:N0}";
+    }
+    private void Update()
+    {
+        money.text = $"{inventoryManager.cash:N0}";
+    }
+    public void rollDice()
+    {
+      float casinoCost= inventoryManager.cash - cost;
+        float reward = cost * 1.5f;
+        inventoryManager.cash = casinoCost;
         int dice1Number = Random.Range(1, 7); // สุ่มเลข 1 ถึง 6 (7 ไม่ถูกรวม)
         int dice2Number = Random.Range(1, 7);
         dice1.text = dice1Number.ToString("F0");
@@ -29,18 +46,22 @@ public class casinoUI : MonoBehaviour
         if(sum%2 == 0 && sum >= 6 && hightEven==true)
         {
             result.text = ("YOU WIN");
+            inventoryManager.cash = inventoryManager.cash + reward;
         }
         else if (sum % 2 != 0 && sum >= 6 && hightOdd == true)
         {
             result.text = ("YOU WIN");
+            inventoryManager.cash = inventoryManager.cash + reward;
         }
         else if (sum % 2 == 0 && sum < 6 && lowEven == true)
         {
             result.text = ("YOU WIN");
+            inventoryManager.cash = inventoryManager.cash + reward;
         }
        else if (sum % 2 != 0 && sum < 6 && lowOdd == true)
         {
             result.text = ("YOU WIN");
+            inventoryManager.cash = inventoryManager.cash + reward;
         }
         else { result.text = ("YOU LOSE"); }
         sum = 0;
@@ -50,18 +71,21 @@ public class casinoUI : MonoBehaviour
         is10000 = true;
         is50000 = false;
         is100000 = false;
+        cost = 10000;
     }
     public void pick50000()
     {
         is10000 = false;
         is50000 = true;
         is100000 = false;
+        cost = 50000;
     }
     public void pick100000()
     {
         is10000 = false;
         is50000 = false;
         is100000 = true;
+        cost = 100000;
     }
 
     public void pickHight_Even()
