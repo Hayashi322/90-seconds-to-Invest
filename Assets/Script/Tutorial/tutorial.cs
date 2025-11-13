e833086c3a9458d1ae998ef752e308fecf0e9561
Phimphisa159 <phimphisa.sris@bumail.net>
2025-11-13 12:58:42 +0700

    add goldshop casino tutorial

---
+++ Assets/Script/Tutorial/tutorial.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class tutorial : MonoBehaviour
+{
+    [SerializeField] private CanvasGroup[] canvases;
+    public void step_1()
+    {
+        canvases[0].alpha = 0 ;
+        canvases[1].alpha = 1;
+        canvases[2].alpha = 0;
+        // canvases[1].enabled = false;
+        // canvases[3].enabled = false;
+    }
+    public void step_2() 
+    {
+        canvases[0].alpha = 0;
+        canvases[1].alpha = 0;
+        canvases[2].alpha = 1;
+    }
+    public void step_3()
+    {
+        close();
+    }
+    public void close ()
+    {
+        for (int i = 0; i < canvases.Length; i++)
+        {
+            canvases[i].gameObject.SetActive(false);
+        }
+    }
+
+}
